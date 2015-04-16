f4ae1fdd77802d2f913af415b688f35aca1a69e8
MartinArroyo <martinarroyo@usal.es>
2015-04-16 15:30:26 +0200

    marcoscp

---
+++ marcoshell/marcoscp.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+function ayuda(){
+	printf 'Usage: %s [-h|-?|-n] [-i [identity_file]] [-p port] [[-o <ssh -o options>] ...] [user]\n' "$0" >&2
+}
+
+while [[ $# -ge 1 ]]
+do
+
+    key="$1"
+
+    case $key in
+	
+        -h)
+        ayuda
+        exit 0
+        shift
+        ;;
+        -d)
+        DIRECTORY="$2"
+        shift
+        ;;
+	-l)
+	USUARIO="$2@"
+	shift
+	shift
+	;;
+	-r)
+	RECURSIVE="-r "
+	;;
+        *)
+        DIRECTORIES="$DIRECTORIES $1"      # opción desconocida
+        shift
+	;;
+esac
+
+done
+echo "Here"
+lista=$(marcodiscover.py  --shell)
+
+for i in $lista; do
+	scp $RECURSIVE $DIRECTORIES $USUARIO$i:$DIRECTORY
+done
+
+exit 0
