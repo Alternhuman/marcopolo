c912de58e65da7c290a0bd3642a2fd3f068c2506
MartinArroyo <martinarroyo@usal.es>
2015-05-10 22:57:51 +0200

    Minor adjustments
    Python2 compatibility
    services

--- marcopolo/bindings/marco/marco.py
+++ marcopolo/bindings/marco/marco.py
@@ -43,11 +43,18 @@ class Marco(object):
     return nodes
 
 
-  def marco(self):
+  def marco(self, max_nodes=None, exclude=[], timeout=None):
     if sys.version_info[0] < 3:
-      self.marco_socket.sendto(bytes(json.dumps({"Command": "Marco"}).encode('utf-8')), ('127.0.1.1', 1338))
+      self.marco_socket.sendto(bytes(json.dumps({"Command": "Marco", 
+                                                 "max_nodes": max_nodes,
+                                                 "exclude":exclude,
+                                                 "timeout":timeout}).encode('utf-8')), ('127.0.1.1', 1338))
     else:
-      self.marco_socket.sendto(bytes(json.dumps({"Command": "Marco"}), 'utf-8'), ('127.0.1.1', 1338))
+      self.marco_socket.sendto(bytes(json.dumps({"Command": "Marco", 
+                                                 "max_nodes": max_nodes,
+                                                 "exclude":exclude,
+                                                 "timeout":timeout}), 'utf-8'), ('127.0.1.1', 1338))
+    
     
     error = None
     try:
@@ -56,11 +63,7 @@ class Marco(object):
       error = True
     if error:
       raise MarcoTimeOutException("No connection to the resolver")
-      # if args.shell:
-      #   print("")
-      # else:
-      #   print("No response from resolver")
-      # exit(1)
+
     error_parse = None
     try:
       nodes = json.loads(data.decode('utf-8'))
@@ -71,6 +74,38 @@ class Marco(object):
       raise MarcoInternalError("Internal parsing error")
     
     return nodes
+
+
+  def services(node, timeout=None):
+    if sys.version_info[0] < 3:
+      self.marco_socket.sendto(bytes(json.dumps({"Command": "Services",
+                                                 "node": node,
+                                                 "timeout":timeout}).encode('utf-8')), ('127.0.1.1', conf.POLOPORT))
+    else:
+      self.marco_socket.sendto(bytes(json.dumps({"Command": "Services",
+                                                 "node": node,
+                                                 "timeout":timeout}), 'utf-8'), ('127.0.1.1', conf.POLOPORT))
+
+    error = None
+    try:
+      data = self.marco_socket.recv(4096)
+    except socket.timeout:
+      error = True
+    
+    if error:
+      raise MarcoTimeOutException("No connection to the resolver")
+
+    error_parse = None
+    try:
+      services_list = json.loads(data.decode('utf-8'))
+    except ValueError:
+      error_parse = True
+    
+    if error_parse:
+      raise MarcoInternalError("Internal parsing error")
+    
+    return services_list
+
 class MarcoTimeOutException(Exception):
   pass
 
