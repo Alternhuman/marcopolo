54686208d0ab0a6326b2badd0dabf200def5b5d5
MartinArroyo <martinarroyo@usal.es>
2015-06-09 23:14:11 +0200

    More testing

--- marcopolo/bindings/java/marcobinding/marcobinding/src/tests/TestMarco.java
+++ marcopolo/bindings/java/marcobinding/marcobinding/src/tests/TestMarco.java
@@ -4,6 +4,13 @@ import org.junit.Test;
 
 import marcobinding.Marco;
 
+/*
+ * Reference
+ * http://www.vogella.com/tutorials/JUnit/article.html
+ * http://zeroturnaround.com/rebellabs/how-to-mock-up-your-unit-test-environment-to-create-alternate-realities/
+ * http://www.jmock.org/
+ * https://www.google.es/search?client=ubuntu&channel=fs&q=mock+junit&ie=utf-8&oe=utf-8&gfe_rd=cr&ei=iVN3VdyLLNiCVIfKgcgI
+ */
 public class TestMarco {
 	@Test
 	public void noConnectionShouldReturnMinusOne(){
