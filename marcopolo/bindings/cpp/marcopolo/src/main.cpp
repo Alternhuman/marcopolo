bc658a05ad4473382593355de965342dfcb01472
MartinArroyo <martinarroyo@usal.es>
2015-06-09 13:49:33 +0200

    Removed some files and request-for working

--- marcopolo/bindings/cpp/marcopolo/src/main.cpp
+++ marcopolo/bindings/cpp/marcopolo/src/main.cpp
@@ -6,14 +6,17 @@ int main(int argc, char* argv[]){
 
 	Marco marco(2000);
 	std::vector<Node> nodes;
+	
+	
+	
 	int attempts = 10;
 
 	while(attempts --> 0){
 
-		marco.marco(nodes);
-		for (int i = 0; i < nodes.size(); ++i)
+		marco.request_for(nodes, "marcobootstrap");
+		for (unsigned int i = 0; i < nodes.size(); ++i)
 		{
-			printf("There is a node at %s whose hostname is %s\n", nodes[i].getAddress().c_str(), nodes[i].getParams()["hostname"].value.c_str());
+			printf("There is a node at %s\n", nodes[i].getAddress().c_str());
 		}
 	}
 
--- marcopolo/bindings/cpp/marcopolo/src/marco.cpp
+++ marcopolo/bindings/cpp/marcopolo/src/marco.cpp
@@ -169,11 +169,7 @@ int Marco::marco(std::vector<Node>& nodes, int max_nodes, std::vector<std::strin
     			perror("Unkwnown error");
     			return -1;
     	}
-    }else{
-        //printf("%s\n", recv_response);
     }
-    
-    
     
     wchar_t to_arr[response];
     char to_arr_aux[response];
@@ -220,176 +216,131 @@ int Marco::marco(std::vector<Node>& nodes, int max_nodes, std::vector<std::strin
     return return_nodes.size();
 }
 
+int Marco::request_for(std::vector<Node>&nodes, std::string service, int max_nodes, std::vector<std::string> exclude, std::map<std::string, std::string> params, int timeout, int retries){
+		timeout = timeout > 0 ? timeout : this->timeout;
+		
+		marco_socket = socket(AF_INET, SOCK_DGRAM, 0);
+		
+		struct timeval timeout_val;  
+	    if (timeout > 0){
+	        timeout_val.tv_sec = 2 * (timeout/1000);
+	        timeout_val.tv_usec = 2 * 1000 * (timeout % 1000);
+	    }
+	    if (setsockopt (marco_socket, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout_val, sizeof(timeout_val)) < 0){
+	        perror("setsockopt failed\n");
+	    }
+		
+		bzero((char *) &(bind_addr), sizeof(bind_addr));
 
-// int Marco::request_for(std::vector<Node>&nodes, std::string service, int max_nodes, std::vector<std::string> exclude, std::map<std::string, std::string> params, int timeout, int retries){
-// 	rapidjson::StringBuffer s;
-// 	rapidjson::Writer<rapidjson::StringBuffer> writer(s);
+		bind_addr.sin_family = AF_INET;
+		bind_addr.sin_port = htons(PORT);
+		bind_addr.sin_addr.s_addr = inet_addr("127.0.1.1");
+		size_addr = sizeof(bind_addr);
 
-// 	timeout = timeout > 0 ? timeout : this->timeout;
 
-// 	writer.StartObject();
-// 	writer.String("Command");
-// 	writer.String("Marco");
-// 	writer.String("max_nodes");
-// 	writer.Uint(max_nodes);
-// 	writer.String("exclude");
-// 	writer.StartArray();
-// 	//TODO
-// 	writer.EndArray();
-// 	//writer.String("params");
-// 	//TODO
-// 	writer.String("timeout");
-// 	writer.Uint(timeout);
-// 	writer.String("group");
-// 	writer.String(this->group.c_str());
-// 	writer.EndObject();
-// 	/*writer.StartObject();
-// 	writer.String("Request-for");
-// 	writer.String("Marco");
-// 	writer.String("Params");
-// 	writer.String(service.c_str());
-// 	writer.String("max_nodes");
-// 	writer.Uint(max_nodes);
-// 	writer.String("exclude");
-// 	writer.StartArray();
-// 	//TODO
-// 	writer.EndArray();
-// 	//writer.String("params");
-// 	//TODO
-// 	writer.String("timeout");
-// 	writer.Uint(timeout);
-// 	writer.String("group");
-// 	writer.String(this->group.c_str());
-// 	writer.EndObject();*/
-
-// 	std::string json_array_tmp(s.GetString());
-// 	//printf("%s\n", json_array_tmp.c_str());
-// 	std::wstring json_array;
-
-// 	json_array = std::wstring(json_array_tmp.begin(), json_array_tmp.end());
-
-// 	const wchar_t *wcs = json_array.c_str();
-// 	signed char utf8[(wcslen(wcs) + 1 /* L'\0' */) * UTF8_SEQUENCE_MAXLEN];
-//     char *iconv_in = (char *) wcs;
-//     char *iconv_out = (char *) &utf8[0];
-//     size_t iconv_in_bytes = (wcslen(wcs) + 1 /* L'\0' */) * sizeof(wchar_t);
-//     size_t iconv_out_bytes = sizeof(utf8);
-    
-//     //size_t ret;
-
-//     size_t b = wchar_to_utf8(wcs, iconv_in_bytes, iconv_out, iconv_out_bytes, 0);
-//     printf("%d\n", b);
-//     if(b < 1){
-//     	perror("Internal error during UTF-8 conversion in request_for");
-//     	//return -1;
-//     }
-
-//     if(-1 == sendto(this->marco_socket,
-//     			    iconv_out,(b/4)-1,
-//     			    0,
-//     			    (struct sockaddr *)&(this->bind_addr),
-//     			    this->size_addr))
-//     {
-//     	return -1;
-//     }
-	
-// 	char recv_response[2048];
-
-//     size_t response = recv(this->marco_socket, recv_response, 2048,0);
-
-//     if(response == -1){
-//     	switch(errno){
-//     		case EINPROGRESS:
-//     		case EAGAIN:
-//     			perror("Timeout\n");
-//     			return -1;
-//     		default:
-//     			perror("Unkwnown error");
-//     			return -1;
-//     	}
-//     }else{
-//         //printf("%s\n", recv_response);
-//     }
-    
-    
-//     wchar_t to_arr[response];
-//     char to_arr_aux[response];
-//     strncpy(to_arr_aux, recv_response, response);
-    
-//     size_t c = utf8_to_wchar(to_arr_aux, response, to_arr, response, 0);
-    
-//     std::wstring ws(to_arr);
-    
-//     std::string str(ws.begin(), ws.end());
-//     str = str.substr(0, c);
-    
-//     rapidjson::Document document;
-    
-//     if(document.Parse<0>(str.c_str()).HasParseError()){
-//     	perror("Internal parsing error");
-//     	return -1;
-// 	}
-
-//     assert(document.IsArray());
-
-//     std::vector<Node> return_nodes;
-
-//     for(int i= 0; i<document.Size();i++){
-//     	Node n;
-//     	n.setAddress(document[i]["Address"].GetString());
-
-// 		std::map<std::string, parameter> params;
-// 		const char* kTypeNames[] =  { "Null", "False", "True", "Object", "Array", "String", "Number" };
-// 		for (rapidjson::Value::ConstMemberIterator itr = document[i]["Params"].MemberBegin(); itr != document[i]["Params"].MemberEnd(); ++itr)
-// 		{
-// 		    parameter p;
-// 			p.type = itr->value.GetType();
-// 			p.value = itr->value.GetString();
-// 			params[itr->name.GetString()] = p;
-// 		}
-// 		n.setParams(params);
+		rapidjson::StringBuffer s;
+		rapidjson::Writer<rapidjson::StringBuffer> writer(s);
 
 		
-//     	return_nodes.push_back(n);
-//     }
+		
+		writer.StartObject();
+		writer.String("Command");
+		writer.String("Request-for");
+		writer.String("Params");
+		writer.String(service.c_str());
+		writer.String("max_nodes");
+		writer.Uint(max_nodes);
+		writer.String("exclude");
+		writer.StartArray();
+		//TODO
+		writer.EndArray();
+		//writer.String("params");
+		//TODO
+		writer.String("timeout");
+		writer.Uint(timeout);
+		writer.String("group");
+		writer.String(this->group.c_str());
+		writer.EndObject();
 
-//     nodes = return_nodes;
-//     return return_nodes.size();
+		std::string json_array_tmp(s.GetString());
+	
+		std::wstring json_array = std::wstring(json_array_tmp.begin(), json_array_tmp.end());
 
-// 	/*  try:
-//          except ValueError as e:
-//             error = True
-//         if error:
-//             raise MarcoTimeOutException("Bad parameters")
+		wchar_t *wcs = (wchar_t*) json_array.c_str();
+		size_t str_size = wcslen(wcs)*sizeof(wchar_t);
+		int str_len = wcslen(wcs);
+		char output[str_len];
 
-//         if rvalue < 1:
-//             raise MarcoInternalError("Error on communication")
+		this->wchar_to_utf8(wcs, output, str_size);
+	    
+	    if(-1 == sendto(marco_socket,
+	    			    output,str_len,
+	    			    0,
+	    			    (struct sockaddr *)&(bind_addr),
+	    			    size_addr))
+	    {
+	    	perror("Error on sending");
+	    	return -1;
+	    }
 
-//         error = None
-//         try:
-//             data, address = self.marco_socket.recvfrom(4096)
-//         except socket.timeout:
-//             error = True
-//         if error:
-//             raise MarcoTimeOutException('No connection to the resolver')
+	    char recv_response[2048];
 
-//         error_parse = None
-//         try:
-//             nodes_arr = json.loads(data.decode('utf-8'))
-//         except ValueError:
-//             error_parse = True
-        
-//         if error_parse:
-//             raise MarcoInternalError("Internal parsing error")
-        
-//         nodes = set()
-//         for node_arr in nodes_arr:
-//             node = Node()
-//             node.address = node_arr["Address"]
-//             node.params = node_arr["Params"]
+	    size_t response = recv(marco_socket, recv_response, 2048,0);
+	    
+	    if(response == (size_t)-1){
+	    	switch(errno){
+	    		case EINPROGRESS:
+	    		case EAGAIN:
+	    			perror("Timeout\n");
+	    			return -1;
+	    		default:
+	    			perror("Unkwnown error");
+	    			return -1;
+	    	}
+	    }
+	    
+	    
+	    
+	    wchar_t to_arr[response];
+	    char to_arr_aux[response];
+	    strncpy(to_arr_aux, recv_response, response);
+	    
+	    if(-1 == this->utf8_to_wchar(to_arr_aux, to_arr, response)){
+	    	perror("Error on conversion");
+	    }
+		
+	    std::wstring ws(to_arr);
+	    std::string str(ws.begin(), ws.end());
+	    
+	    rapidjson::Document document;
+	    
+	    if(document.Parse<0>(str.c_str()).HasParseError()){
+	    	perror("Internal parsing error");
+	    	return -1;
+		}
+		
+	    assert(document.IsArray());
 
-//             nodes.add(node)
-//         return nodes*/
+	    std::vector<Node> return_nodes;
 
-// 	return 0;
-// }
+	    for(unsigned int i= 0; i<document.Size();i++){
+	    	Node n;
+	    	n.setAddress(document[i]["Address"].GetString());
+
+			std::map<std::string, parameter> params;
+			for (rapidjson::Value::ConstMemberIterator itr = document[i]["Params"].MemberBegin(); itr != document[i]["Params"].MemberEnd(); ++itr)
+			{
+			    parameter p;
+				p.type = itr->value.GetType();
+				p.value = itr->value.GetString();
+				params[itr->name.GetString()] = p;
+			}
+			n.setParams(params);
+
+			
+	    	return_nodes.push_back(n);
+	    }
+	    nodes = return_nodes;
+	    close(marco_socket);
+	    return return_nodes.size();
+}
