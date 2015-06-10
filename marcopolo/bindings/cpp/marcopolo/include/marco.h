1cb57c556b4550c63317ed646f9dd44fa55add4c
MartinArroyo <martinarroyo@usal.es>
2015-06-10 09:44:10 +0200

    C test

--- marcopolo/bindings/cpp/marcopolo/include/marco.h
+++ marcopolo/bindings/cpp/marcopolo/include/marco.h
@@ -17,9 +17,13 @@ typedef struct node_t{
 	parameter_t* params;
 }node_t;
 
+#ifdef __cplusplus
 extern "C"{
+#endif
 	int marco(marco_t mp, node_t** nodes, int max_nodes, char* exclude[], int exclude_len, parameter_t* params, int params_len, int timeout, int retries);
 	int request_for(marco_t mp, char* service, node_t** nodes, int max_nodes, char* exclude[], int exclude_len, parameter_t* params, int params_len, int timeout, int retries);
+#ifdef __cplusplus
 }
+#endif
 
 #endif
---
+++ marcopolo/bindings/cpp/marcopolo/marcotest/build/CMakeFiles/marcotest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/marcotest.dir/src/main.c.o"
+  "marcotest.pdb"
+  "marcotest"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/marcotest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
