77b0527f5c91b97481a55c663844d3252252a6c6
MartinArroyo <martinarroyo@usal.es>
2015-06-11 13:31:56 +0200

    WAT

---
+++ marcopolo/daemon/python3/polostop.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8
+from os import kill, remove
+import os, signal
+
+BINARY = '/opt/marcopolo/polo/polod.py'
+PIDFILE = '/var/run/marcopolo/polod.pid'
+
+try:
+    f = open(PIDFILE, 'r')
+    pid = f.read()
+    f.close()
+    kill(int(pid), signal.SIGTERM)
+    os.remove(PIDFILE)
+except Exception as e:
+    print(e)
